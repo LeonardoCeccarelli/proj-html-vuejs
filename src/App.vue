ffa79cd6b1097838293ba9c77b579b582d1dd973
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-22 13:27:03 +0100

    add header (vue and scss)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <Header></Header>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Header },
 };
 </script>
 
 <style lang="scss">
+@import "./style/app.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <nav>
+      <!-- Sezione logo -->
+      <div class="nav_logo">
+        <div class="logo_container">
+          <img src="@/assets/dark-logo.png" alt="text logo" />
+        </div>
+      </div>
+      <!-- Sezione lista link -->
+      <div class="nav_link">
+        <ul>
+          <!-- Singolo link -->
+          <li v-for="(link, i) in linkNavList" :key="i">
+            <a :href="link.url">{{ link.text }}</a>
+            <i class="fa fa-angle-down"></i>
+          </li>
+        </ul>
+      </div>
+      <!-- Sezione settaggi -->
+      <div class="nav_search">
+        <div class="nav_search_language">
+          <!-- Logo bandiera -->
+          <div class="image_container">
+            <img src="@/assets/en.png" alt="flag logo" />
+          </div>
+          <!-- Testo lingua -->
+          <div class="language_container">
+            <a href="#">English</a>
+            <i class="fa fa-angle-down"></i>
+          </div>
+          <!-- Icona profilo -->
+          <div class="user_icon_container">
+            <i class="fa fa-user-circle-o"></i>
+          </div>
+        </div>
+        <div class="nav_search_input">
+          <input type="text" placeholder="Search..." />
+          <i class="fa fa-search"></i>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      linkNavList: [
+        {
+          text: "home",
+          url: "#home",
+        },
+        {
+          text: "pages",
+          url: "#pages",
+        },
+        {
+          text: "Courses",
+          url: "#Courses",
+        },
+        {
+          text: "Features",
+          url: "#Features",
+        },
+        {
+          text: "blog",
+          url: "#blog",
+        },
+        {
+          text: "shop",
+          url: "#shop",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
