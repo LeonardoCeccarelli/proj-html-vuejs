6d9a3957b227c8a17e43f353f337bd94409a9fef
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-22 18:50:20 +0100

    add course section

---
+++ src/components/CourseMain.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="course_container">
+      <div class="course_content">
+        <div class="course_title">
+          <p class="m_bottom">Let's Dream Big Together</p>
+          <h2>Explore How Cain I Help You</h2>
+        </div>
+        <ul class="course_card_container">
+          <li v-for="(card, i) in listCourseCard" :key="i" class="course_card">
+            <div class="content_card">
+              <img :src="require('@/assets/' + card.imagePath)" alt="" />
+              <div class="content_card_text">
+                <p class="price">
+                  {{ card.priceInt
+                  }}<small v-if="card.priceFloat">{{ card.priceFloat }}</small>
+                </p>
+                <p class="description m_bottom">
+                  {{ card.title }}
+                </p>
+                <div class="on_hover_card">
+                  <div class="info">
+                    <span
+                      ><i class="fa fa-file-text-o" aria-hidden="true"></i> 20
+                      Lessons</span
+                    >
+                    <span
+                      ><i class="fa fa-user-o" aria-hidden="true"></i> 50
+                      Students</span
+                    >
+                    <p class="info_description">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Tenetur omnis itaque quasi, quam nobis beatae officiis...
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseMain",
+  data() {
+    return {
+      listCourseCard: [
+        {
+          imagePath: "course-02-443x600.jpg",
+          priceInt: "$40",
+          priceFloat: ".00",
+          title: "Learning to Write as a Professional Author",
+        },
+        {
+          imagePath: "stock-full-hd-03-443x600.jpg",
+          priceInt: "Free",
+          priceFloat: false,
+          title: "Customer-Centric Info-Tech Strategies",
+        },
+        {
+          imagePath: "stock-full-hd-04-443x600.jpg",
+          priceInt: "$19",
+          priceFloat: ".00",
+          title: "Open Programming Courses for Everyone: Phyton",
+        },
+        {
+          imagePath: "stock-full-hd-06-443x600.jpg",
+          priceInt: "$26",
+          priceFloat: ".00",
+          title: "Academic LIstening and Note-taking",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,15 +3,17 @@
     <MainHero></MainHero>
     <InfoMain></InfoMain>
     <HelpMain></HelpMain>
+    <CourseMain></CourseMain>
   </div>
 </template>
 
 <script>
+import CourseMain from "./CourseMain.vue";
 import HelpMain from "./HelpMain.vue";
 import InfoMain from "./InfoMain.vue";
 import MainHero from "./MainHero.vue";
 export default {
-  components: { MainHero, InfoMain, HelpMain },
+  components: { MainHero, InfoMain, HelpMain, CourseMain },
   name: "Main",
 };
 </script>
