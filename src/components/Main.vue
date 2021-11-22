a624227175ede890220907159c27c6797ab2840d
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-22 15:10:58 +0100

    add hero section

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <MainHero></MainHero>
+    <InfoMain><InfoMain>
+  </div>
+</template>
+
+<script>
+import InfoMain from "./InfoMain.vue";
+import MainHero from "./MainHero.vue";
+export default {
+  components: { MainHero, InfoMain },
+  name: "Main",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/MainHero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero_section_container">
+    <div class="hero_section_content">
+      <div class="text_container">
+        <div>
+          <p class="m_bottom head_title">New Challenge, New Skills</p>
+          <!-- Titolo -->
+          <h1 class="m_bottom">Build your own life coaching buisness</h1>
+          <p class="m_bottom sub_title">
+            Whole-Life Buisness Coaching for committed enterpreneurs
+          </p>
+          <button class="btun btun_primary" type="button">
+            Get started today
+          </button>
+        </div>
+      </div>
+      <div class="image_container">
+        <img
+          src="@/assets/home-business-hero-global-image.png"
+          alt="global image"
+          class="background_global"
+        />
+        <img src="@/assets/home-business-hero-avatar.png" alt="avatar image" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHero",
+};
+</script>
+
+<style>
+</style>
